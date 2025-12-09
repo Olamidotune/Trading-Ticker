192dd8c5211ec4fc801a9f6709c581aa19275866
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2025-12-09 13:55:11 +0100

    feat: add environment variable setup and update API keys

--- lib/services/keys.dart
+++ lib/services/keys.dart
@@ -1,6 +1,7 @@
 final Map<String, dynamic> baseHeaders = {'Accept': 'application/json'};
 
 class EnvKeys {
-  static const String apiBaseUrl = 'API_BASE_URL';
+  static const String apiBaseUrl = 'BASE_URL';
   static const String newsApiBaseUrl = 'NEWS_API_BASE_URL';
+  static const String newsApiKey = 'NEWS_API_KEY';
 }
