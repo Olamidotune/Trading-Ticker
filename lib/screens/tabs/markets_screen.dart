47159972fa15d21e09f6dd21d76691662f7ffe73
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-01-06 01:04:14 +0100

    feat: add VolumeCard widget for displaying coin volume information

--- lib/screens/tabs/markets_screen.dart
+++ lib/screens/tabs/markets_screen.dart
@@ -2,6 +2,7 @@ import 'package:cointicker/bloc/coin/crypto_bloc.dart';
 import 'package:cointicker/constants/app_colors.dart';
 import 'package:cointicker/widgets/coin_card.dart';
 import 'package:cointicker/widgets/coin_details_sheet.dart';
+import 'package:cointicker/widgets/volume.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
