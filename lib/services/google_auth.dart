6a512ca4cbbe90506ccf681477d189a8b8d275fe
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-01-14 00:13:14 +0100

    fix: optimize the google auth

--- lib/services/google_auth.dart
+++ lib/services/google_auth.dart
@@ -1,6 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cointicker/services/logging_helper.dart';
-import 'package:cointicker/services/persistence_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -15,58 +12,8 @@ class GoogleSignInService {
             '839188101886-v45mfn5s4v3tfv7aut9pktmq2aamubar.apps.googleusercontent.com');
   }
 
-  Future<User?> signInWithGoogle() async {
-    try {
-      await initSignIn();
-
-      final googleUser = await _googleSignIn.authenticate();
-
-      final googleAuth = googleUser.authentication;
-
-      final credential = GoogleAuthProvider.credential(
-        idToken: googleAuth.idToken,
-      );
-
-      final userCredential = await _auth.signInWithCredential(credential);
-
-      final isNewUser = userCredential.additionalUserInfo?.isNewUser ?? false;
-
-      if (isNewUser) {
-        logInfo('New user signed up with Google');
-
-        final uid = userCredential.user!.uid;
-        final fullName = googleUser.displayName;
-        final email = googleUser.email;
-
-        await FirebaseFirestore.instance.collection('users').doc(uid).set({
-          'fullName': fullName,
-          'email': email,
-          'createdAt': FieldValue.serverTimestamp(),
-        });
-
-        await PersistenceService().saveUserName(fullName ?? '');
-        await PersistenceService().saveUserEmail(email);
-      } else {
-        logInfo('Existing user logged in with Google');
-
-        final fullName = userCredential.user?.displayName;
-        final email = userCredential.user?.email ?? '';
-
-        await PersistenceService().saveUserName(fullName ?? '');
-        await PersistenceService().saveUserEmail(email);
-      }
-
-      return userCredential.user;
-    } catch (e, stack) {
-      logError('Google auth error: $e', stack);
-      return null;
-    }
-  }
-
   static Future<void> signOut() async {
     await _googleSignIn.signOut();
     await _auth.signOut();
   }
-
-  static User? getCurrentUser() => _auth.currentUser;
 }
