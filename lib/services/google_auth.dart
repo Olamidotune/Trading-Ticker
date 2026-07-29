fa8b0cb1b0e48ed9caa98b7036dfa0e7034f43d2
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-07-30 00:34:25 +0100

    fix: add missing imports for Firestore and services in GoogleSignInService

--- lib/services/google_auth.dart
+++ lib/services/google_auth.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cointicker/services/logging_helper.dart';
+import 'package:cointicker/services/persistence_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
